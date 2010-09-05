5d22263fbdab1eee105f203db6efc9ea0ef74d31
stonean <andy@stonean.com>
2010-09-04 21:53:15 -0400

    migrated some lockdown:rules methods to configuration.  added test

--- lib/lockdown/configuration.rb
+++ lib/lockdown/configuration.rb
@@ -3,9 +3,6 @@
 module Lockdown
   module Configuration
     class << self
-      # Path to redirect to if access is denied. 
-      # Default: '/'        
-      attr_accessor :access_denied_path
       # Regex string of paths that are publicly accessible. 
       # Default "\/"
       attr_accessor :public_access
@@ -15,6 +12,9 @@ module Lockdown
       # Array of permission objects that defines the access to the application.
       # Default []
       attr_accessor :permissions
+      # Array of user group objects
+      # Default []
+      attr_accessor :user_groups
       # Method used to get the id of the user responsible for 
       # the current action.
       # Default :current_user_id
@@ -22,12 +22,15 @@ module Lockdown
       # User id to associate to system actions
       # Default 1
       attr_accessor :default_who_did_it
-      # Redirect to path on access violation
-      # Default "/"
+      # Path to redirect to if access is denied.
+      # Default: '/'
       attr_accessor :access_denied_path
       # Redirect to path on successful login
       # Default "/"
       attr_accessor :successful_login_path
+      # Logout user if attempt to access restricted resource
+      # Default false
+      attr_accessor :logout_on_access_violation
       # When using the links helper, this character will be 
       # used to separate the links.
       # Default "|"
@@ -43,27 +46,131 @@ module Lockdown
 
       # Set defaults.
       def reset
-        @access_denied_path           = "/"
         @public_access                = ""
         @protected_access             = ""
         @permissions                  = []
+        @user_groups                  = []
 
         @who_did_it                   = :current_user_id
         @default_who_did_it           = 1
-        @logout_on_access_violation   = false
+
         @access_denied_path           = "/"
         @successful_login_path        = "/"
+        @logout_on_access_violation   = false
 
         @link_separator               = "|"
 
-        @user_group_model             = "UserGroup",
+        @user_group_model             = "UserGroup"
         @user_model                   = "User"
       end
 
+      # @return [String] concatentation of public_access + "|" + protected_access
+      def authenticated_access
+        public_access + "|" + protected_access
+      end
+
+      # @param [String,Symbol] name permission name
+      # @return Lockdown::Permission object 
+      def permission(name)
+        name = name.to_s
+        perm = permissions.detect{|perm| name == perm.name}
+        raise PermissionNotFound unless perm
+        perm
+      end
+
+      # Defines the permission as public
+      # @param [String,Symbol] name permission name
+      def make_permission_public(name)
+        permission(name).is_public
+      end
+
+      # Defines the permission as protected
+      # @param [String,Symbol] name permission name
+      def make_permission_protected(name)
+        permission(name).is_protected
+      end
+
+      # @return Array of permission names
+      def permission_names
+        permissions.collect{|p| p.name}
+      end
+
+      # @param [Lockdown::Permission] permission Lockdown::Permission object
+      # @return [true|false] true if object exists with same name
       def has_permission?(permission)
         permissions.any?{|p| permission.name == p.name}
       end
-    end
+
+      # @param [String|Symbol] name permission name
+      # @return [true|false] true if permission is either public or protected
+      def permission_assigned_automatically?(name)
+        name = name.to_s
+
+        perm = permission(name)
+
+        perm.public? || perm.protected?
+      end
+
+      # @param [String,Symbol] name user group name
+      # @return [Lockdown::UserGroup] object 
+      def user_group(name)
+        name = name.to_s
+        user_groups.detect{|ug| name == ug.name}
+      end
+
+      # @return [Lockdown::UserGroup] 
+      def find_or_create_user_group(name)
+        name = name.to_s
+        user_group(name) || Lockdown::UserGroup.new(name)
+      end
+
+      # @return [Array] names
+      def user_group_names
+        user_groups.collect{|ug| ug.name}
+      end
+
+      # @param [String] name user group name
+      # @return [Array] permissions names 
+      def user_group_permissions_names(name)
+        user_group(name).permissions.collect{|p| p.name}
+      end
+
+      # @return [True|False] true if user has 'Administrators' group 
+      def administrator?(user)
+        user_has_user_group?(user, Lockdown.administrator_group_name)
+      end
+
+      # @param [User] user User object you want to make an administrator
+      def make_user_administrator(user)
+        user_groups = user.send(Lockdown.user_groups_hbtm_reference)
+        user_groups << Lockdown.user_group_class.
+          find_or_create_by_name(Lockdown.administrator_group_name)
+      end
+
+
+      # @param [User, String] user,name  user model, name of user group
+      # @return [True|False] true if user has user group with name
+      def user_has_user_group?(user, name)
+        user_groups = user.send(Lockdown.user_groups_hbtm_reference)
+        user_groups.any?{|ug| name == ug.name}
+      end
+
+      # @return [Regex] 
+      def access_rights_for_user(user)
+        return unless user
+        return Lockdown::Resource.regex if administrator?(user)
+
+        rights = authenticated_access
+
+        user_groups = user.send(Lockdown.user_groups_hbtm_reference)
+        user_groups.each do |grp|
+          user_group(grp.name).permissions.each do |perm|
+            rights += "|(#{perm.regex_pattern})"
+          end
+        end
+        rights
+      end
+    end # class block
 
     self.reset
   end # Configuration
